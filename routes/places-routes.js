0ea75172c2d3c69a29fa22ff4a06adb2f1300520
hafiz1379 <hafizrasa1379@gmail.com>
2024-02-07 19:14:24 +0430

    Update places-routes.js with new routes and controllers

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,9 +1,16 @@
 const express = require("express");
-const placesControllers = require("../controllers/places-controller");
+
+const placesControllers = require("../controllers/places-controllers");
+
 const router = express.Router();
 
 router.get("/:pid", placesControllers.getPlaceById);
 
 router.get("/user/:uid", placesControllers.getPlaceByUserId);
+
+router.post("/", placesControllers.createPlace);
+
+router.patch("/:pid", placesControllers.updatePlace);
+router.delete("/:pid", placesControllers.deletePlace );
 
 module.exports = router;
