620f37a5d52d8addb9a21560ab596ba23f2bdaab
hafiz1379 <hafizrasa1379@gmail.com>
2024-02-08 10:11:38 +0430

    Add users routes to app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const placesRoutes = require('./routes/places-routes');
+const usersRoutes = require('./routes/users-routes');
+
 const HttpError = require('./models/http-error');
 
 const app = express();
@@ -9,6 +11,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use('/api/places', placesRoutes); // => /api/places...
+app.use('/api/users', usersRoutes); // => /api/users...
 
 app.use((req, res, next) => {
   const error = new HttpError('Could not find this route.', 404);
