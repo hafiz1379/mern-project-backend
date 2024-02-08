c0fef58790662a17208126bf4a3ff854caabddbb
hafiz1379 <hafizrasa1379@gmail.com>
2024-02-08 10:11:26 +0430

    Refactor users-routes.js to use express-validator and add signup and login routes

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,1 +1,16 @@
-// users routes for the app
+const express = require("express");
+const { check } = require("express-validator");
+const usersControllers = require("../controllers/users-controllers");
+const router = express.Router();
+
+router.get("/", usersControllers.getUsers);
+
+router.post("/signup",[
+    check("name").not().isEmpty(),
+    check("email").normalizeEmail().isEmail(),
+    check("password").isLength({min: 6})
+], usersControllers.signup);
+
+router.post("/login", usersControllers.login);
+
+module.exports = router;
