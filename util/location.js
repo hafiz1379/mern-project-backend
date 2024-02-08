4ec760e17bb3069c50495763d2d88b2862c106b5
hafiz1379 <hafizrasa1379@gmail.com>
2024-02-08 20:03:08 +0430

    Update API key in location.js

--- util/location.js
+++ util/location.js
@@ -2,28 +2,43 @@ const axios = require('axios');
 
 const HttpError = require('../models/http-error');
 
-const API_KEY = 'AIzaSyDgLmMpKCzveJf1_yuA0fUzzhy0WRChvZA';
+const API_KEY = 'AIzaSyDORT2sxCKSBIX-qAlmxFTPP56hjTvz4w4';
 
 async function getCoordsForAddress(address) {
-  const response = await axios.get(
-    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
-      address
-    )}&key=${API_KEY}`
-  );
-
-  const data = response.data;
-
-  if (!data || data.status === 'ZERO_RESULTS') {
-    const error = new HttpError(
-      'Could not find location for the specified address.',
-      422
+  try {
+    const response = await axios.get(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+        address
+      )}&key=${API_KEY}`
     );
-    throw error;
+
+    console.log('Response data:', response.data);
+
+    const data = response.data;
+
+    if (!data || data.status === 'ZERO_RESULTS') {
+      throw new HttpError(
+        'Could not find location for the specified address.',
+        422
+      );
+    }
+
+    const coordinates = data.results[0]?.geometry?.location;
+
+    if (!coordinates) {
+      throw new HttpError(
+        'Could not extract coordinates from the response data.',
+        422
+      );
+    }
+
+    return coordinates;
+  } catch (error) {
+    console.error('Error fetching coordinates:', error);
+    throw new HttpError(
+      'Something went wrong, could not fetch coordinates for the specified address.',
+      500
+    );
   }
-
-  const coordinates = data.results[0].geometry.location;
-
-  return coordinates;
 }
-
-module.exports = getCoordsForAddress;
+module.exports = getCoordsForAddress; // Exporting the function
