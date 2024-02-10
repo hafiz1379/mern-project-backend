c0cf10f107702385d0d8d9675588aa46694db57a
hafiz1379 <hafizrasa1379@gmail.com>
2024-02-10 16:31:22 +0430

    Update mongoose import and modify creator field in place schema

--- models/place.js
+++ models/place.js
@@ -1,6 +1,7 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
+
 
 const placeSchema = new Schema({
     title: { type: String, required: true },
@@ -11,7 +12,7 @@ const placeSchema = new Schema({
         lat: { type: Number, required: true },
         lng: { type: Number, required: true },
     },
-    creator: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
-    });
+    creator: { type: String, required: true }
+});
 
-module.exports = mongoose.model("Place", placeSchema);
+module.exports = mongoose.model('Place', placeSchema);
