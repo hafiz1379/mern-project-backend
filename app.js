f1eefea2f58aef792b80e8cc93da7913b44e47e3
hafiz1379 <hafizrasa1379@gmail.com>
2024-02-11 09:58:10 +0430

    Refactor route paths in app.js

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use("/api/places", placesRoutes); // => /api/places...
-app.use("/api/users", usersRoutes); // => /api/users...
+app.use("/api/places", placesRoutes); 
+app.use("/api/users", usersRoutes); 
 
 app.use((req, res, next) => {
   const error = new HttpError("Could not find this route.", 404);
