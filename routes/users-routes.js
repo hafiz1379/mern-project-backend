dc6ec9c41935794759a5d405b63da20c8802288c
hafiz1379 <hafizrasa1379@gmail.com>
2024-02-13 09:44:00 +0430

    Refactor users-routes.js file

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,16 +1,28 @@
-const express = require("express");
-const { check } = require("express-validator");
-const usersControllers = require("../controllers/users-controllers");
+const express = require('express');
+const { check } = require('express-validator');
+
+const usersController = require('../controllers/users-controllers');
+const fileUpload = require('../middleware/file-upload');
+
 const router = express.Router();
 
-router.get("/", usersControllers.getUsers);
+router.get('/', usersController.getUsers);
 
-router.post("/signup",[
-    check("name").not().isEmpty(),
-    check("email").normalizeEmail().isEmail(),
-    check("password").isLength({min: 6})
-], usersControllers.signup);
+router.post(
+  '/signup',
+  fileUpload.single('image'),
+  [
+    check('name')
+      .not()
+      .isEmpty(),
+    check('email')
+      .normalizeEmail()
+      .isEmail(),
+    check('password').isLength({ min: 6 })
+  ],
+  usersController.signup
+);
 
-router.post("/login", usersControllers.login);
+router.post('/login', usersController.login);
 
 module.exports = router;
